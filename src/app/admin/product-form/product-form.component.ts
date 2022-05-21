2f7d5e0af95ce4e0bbf6de304f0edc8e9cd8c681
badeai <90259268+badeai@users.noreply.github.com>
2022-05-21 04:09:34 +0300

    3

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,31 +1,16 @@
 import { CategoryService } from './../../category.service';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.css'],
 })
-// export class ProductFormComponent implements OnInit {
-//   categories!: Observable<any>[];
-//   constructor(categoryService: CategoryService) {
-//     this.categories = categoryService.getCategories();
-//   }
-
-//   ngOnInit() {}
-// }
 export class ProductFormComponent implements OnInit {
-  categories: Observable<any[]> | undefined;
-
-  constructor(
-    categoryService: CategoryService,
-    private db: AngularFireDatabase
-  ) {
+  categories;
+  constructor(categoryService: CategoryService) {
     this.categories = categoryService.getCategories();
   }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
+
+  ngOnInit(): void {}
 }
