881c3b0ce36fb19c0e318808b0fc54afee7d75d4
badeai <90259268+badeai@users.noreply.github.com>
2022-05-21 03:58:23 +0300

    firebase sux 2

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,16 +1,31 @@
 import { CategoryService } from './../../category.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.css'],
 })
-export class ProductFormComponent implements OnInit {
-  categories$;
-  constructor(categoryService: CategoryService) {
-    this.categories$ = categoryService.getCategories();
-  }
+// export class ProductFormComponent implements OnInit {
+//   categories!: Observable<any>[];
+//   constructor(categoryService: CategoryService) {
+//     this.categories = categoryService.getCategories();
+//   }
 
-  ngOnInit() {}
+//   ngOnInit() {}
+// }
+export class ProductFormComponent implements OnInit {
+  categories: Observable<any[]> | undefined;
+
+  constructor(
+    categoryService: CategoryService,
+    private db: AngularFireDatabase
+  ) {
+    this.categories = categoryService.getCategories();
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -1,11 +1,32 @@
-import { AngularFireDatabase } from '@angular/fire/compat/database';
+import {
+  AngularFireDatabase,
+  AngularFireList,
+} from '@angular/fire/compat/database';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/internal/operators/map';
 
 @Injectable()
+// export class CategoryService {
+//   constructor(private db: AngularFireDatabase) {}
+//   getCategories() {
+//     return this.db.list('/categories');
+//     // .valueChanges();
+//aflCategories!: AngularFireList<any>;
+// }
 export class CategoryService {
+  aflCategories: AngularFireList<any> | undefined;
   constructor(private db: AngularFireDatabase) {}
+
   getCategories() {
-    return this.db.list('/categories');
-    // .valueChanges();
+    this.aflCategories = this.db.list('/categories', (category) =>
+      category.orderByChild('name')
+    );
+    return this.aflCategories
+      .snapshotChanges()
+      .pipe(
+        map((changes: any[]) =>
+          changes.map((c) => ({ key: c.payload.key, ...c.payload.val() }))
+        )
+      );
   }
 }
